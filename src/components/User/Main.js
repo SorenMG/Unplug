8e339a843d9d3808135257ca31a9989d2ea3ce67
Søren Møller Gade Hansen <>
2021-03-12 15:10:55 +0100

    .

--- src/components/User/Main.js
+++ src/components/User/Main.js
@@ -17,6 +17,7 @@ import { errorMessages } from '../../constants/messages';
 import Constants from 'expo-constants';
 import listBuilder from '../../lib/list-builder';
 import { Actions } from 'react-native-router-flux';
+import JobCell from '../Jobs/JobCell';
 
 const SettingsStructure = [
   {
@@ -68,24 +69,19 @@ const MainUser = ({ error, loading }) => {
           <Text>Upcomming work</Text>
         </Separator>
         <View style={{ backgroundColor: '#FFFFFF' }}>
-          <ListItem thumbnail last>
-            <Left>
-              <Thumbnail
-                square
-                source={{ uri: 'https://www.faerch.com/Content/2270314.png?maxwidth=800' }}
-                style={{ borderRadius: 10 }}
-              />
-            </Left>
-            <Body>
-              <Text>Titel</Text>
-              <Text note>Beskrivelse</Text>
-            </Body>
-            <Right>
-              <Text note>120</Text>
-
-              <Text note>pr. time</Text>
-            </Right>
-          </ListItem>
+          {JobCell(
+            {
+              id: 1,
+              title: 'Faerch Plast',
+              description: 'Arbejde med indpakning, transport osv.',
+              price: 120,
+              imageUri: 'https://www.faerch.com/Content/2270314.png?maxwidth=800',
+              startTime: '2020-03-07T12:00:00',
+              endTime: '2020-03-07T15:00:00',
+              address: 'Nellikevej 10B, 8240 Risskov',
+            },
+            () => Actions.jobsSingle({ id: 1, title: 'Faerch Plast' }),
+          )}
         </View>
         {listBuilder(SettingsStructure)}
         <Separator />
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { Router, Stack } from 'react-native-router-flux';
 import { PersistGate } from 'redux-persist/es/integration/react';
+import * as firebase from 'firebase';
 
 import { Root, StyleProvider } from 'native-base';
 import getTheme from '../native-base-theme/components';
@@ -16,6 +17,19 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = { loading: true };
+
+    const firebaseConfig = {
+      apiKey: 'api-key',
+      authDomain: 'project-id.firebaseapp.com',
+      databaseURL: 'https://project-id.firebaseio.com',
+      projectId: 'project-id',
+      storageBucket: 'project-id.appspot.com',
+      messagingSenderId: 'sender-id',
+      appId: 'app-id',
+      measurementId: 'G-measurement-id',
+    };
+
+    firebase.initializeApp(firebaseConfig);
   }
 
   async componentDidMount() {
